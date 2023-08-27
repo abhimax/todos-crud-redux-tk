18cc9d440c4063f2a081d24bb2a68ac2d71eab13
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-08-27 15:37:56 +0530

    feat: updated task list to with crud

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,21 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchTasks } from "../store/slices/taskSlice";
+import { fetchTasks, addTask, deleteTask } from "../store/slices/taskSlice";
 
 const TaskList = () => {
   const tasks = useSelector((state) => state.tasks);
   const dispatch = useDispatch();
+  const [newTaskTitle, setNewTaskTitle] = useState("");
 
   useEffect(() => {
     dispatch(fetchTasks());
   }, [dispatch]);
 
+  const handleAddTask = () => {
+    if (newTaskTitle.trim() !== "") {
+      dispatch(
+        addTask({ id: Date.now(), title: newTaskTitle, completed: false })
+      );
+      setNewTaskTitle("");
+    }
+  };
+
+  const handleDeleteTask = (taskId) => {
+    dispatch(deleteTask(taskId));
+  };
+
   return (
-    <div>
+    <div className="task-container">
+      <h2>Task List</h2>
+      <input
+        type="text"
+        value={newTaskTitle}
+        onChange={(e) => setNewTaskTitle(e.target.value)}
+        placeholder="Enter new task title"
+      />
+      <button onClick={handleAddTask}>Add Task</button>
       <ul>
         {tasks.map((task) => (
           <li key={task.id}>
-            {task.title} - {task.completed ? "Completed" : "Incomplete"}
+            {task.title}
+            <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
           </li>
         ))}
       </ul>
